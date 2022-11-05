e376e01d5565daa64c54b476b8b19374829c17c8
swimmingkiim <swimmingkiim@gmail.com>
2022-11-05 17:32:46 +0900

    Add test file

--- packages/dart_swagger_model/example/dart_swagger_model_example.dart
+++ packages/dart_swagger_model/example/dart_swagger_model_example.dart
@@ -1,1 +1,11 @@
-void main() {}
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:dart_swagger_model/dart_swagger_model.dart';
+
+void main() async {
+  final file = File('./example/test.json');
+  final testJson = jsonDecode(file.readAsStringSync());
+  final OpenApi openApi = OpenApi.fromJson(testJson);
+  print(openApi.toJson());
+}
