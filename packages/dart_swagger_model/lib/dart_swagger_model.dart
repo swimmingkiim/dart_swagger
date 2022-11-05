b443bd806dbb070843c6bd41e6c27a5d86c07b94
swimmingkiim <swimmingkiim@gmail.com>
2022-11-05 17:32:28 +0900

    Update barrel file

--- packages/dart_swagger_model/lib/dart_swagger_model.dart
+++ packages/dart_swagger_model/lib/dart_swagger_model.dart
@@ -1,1 +1,9 @@
 library dart_swagger_model;
+
+// OpenApi related
+export 'src/open_api.dart';
+
+// Info related
+export 'src/info/info.dart';
+export 'src/info/contact/contact.dart';
+export 'src/info/license/license.dart';
